a7ddde4c9c714d44ddb90a22e429f30a2e943fac
Nino.Swag <nino.chang@swag.live>
2022-07-09 14:35:48 +0800

    sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -l
 
-echo "Hello $1, $2"
-./cloudflared --no-autoupdate run --token $1 
-# sleep $2
+# echo "::set-output name=time::$time"
+./cloudflared --no-autoupdate run --token $1 && sleep $2
 
 # time=$(date)
 # echo "::set-output name=time::$time"
