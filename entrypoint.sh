333b4e02d60a670c35381b879eead4f7da43fed4
Nino.Swag <nino.chang@swag.live>
2022-07-09 14:52:05 +0800

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "::set-output name=hello"
 # ./cloudflared --no-autoupdate run --token $1 && sleep $2
-# ./cloudflared version
+./cloudflared version && echo "::set-output name=qqq:bbb"
 
 # time=$(date)
 # echo "::set-output name=time::$time"
